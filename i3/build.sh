d2fca86abb1979dca143ca7aeb56d18a613e617b
gInf1e <kut.slava09@gmail.com>
2018-02-04 23:15:22 +0300

    Added i3 dir and build.sh script

---
+++ i3/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Inf1e's build file, you may need personal edit
+
+# Clean conf.d, if exists
+if [ -d "~/.config/i3/conf.d" ]; then
+	rm ~/.config/i3/conf.d/*
+	echo "Cleaned conf.d"
+fi
+
+# Copy config files from cloned repository
+cp ./conf.d/* ~/.config/i3/conf.d/
+
+# Merge conf.d into single config
+cat ~/.config/i3/conf.d/* > ~/.config/i3/config
